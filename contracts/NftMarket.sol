038eccba12811f376b58302ebe930d14b72bac3c
vugomars <vugomars@gmail.com>
2022-09-03 16:54:44 +0700

    18 - create mintToken NFT

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,8 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NftMarket is ERC721 {
+contract NftMarket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _listedItems; // 0
+    Counters.Counter private _tokenIds; // 0
+
+
     constructor() ERC721("VugomarsNFT", "VGM") {}
+
+
+    function mintToken(string memory tokenURI) public payable returns (uint) {
+        _listedItems.increment(); // 1
+        _tokenIds.increment(); // 1
+
+        uint newTokenId = _tokenIds.current(); //1
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
+    }
 }
